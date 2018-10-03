016b66d9248c8de6ee5719e1dc354699446a2f3c
Augusto Hubert <augustohub@gmail.com>
2018-10-03 18:06:01 -0300

    Initial test for BasicHealthUnit model

---
+++ spec/models/basic_health_unit_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe BasicHealthUnit, type: :model do
+  subject {
+    described_class.new(name: 'AMBULATORIO SANTA ANA',
+                        address: 'LINHA BOA ESPERANCA, DOBRADA',
+                        city: 'Sertão Santana',
+                        phone: '5134951206',
+                        latitude: '-30.4481470584861',
+                        longitude: '-51.6436600685105',
+                        unit_size_score: 3,
+                        adaptation_for_seniors_score: 1,
+                        medical_equipment_score: 2,
+                        medicine_score: 2)
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+end
