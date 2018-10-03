4d2e1e511130d9e9e10bb2240ad24ce7739ff680
Augusto Hubert <augustohub@gmail.com>
2018-10-03 19:24:36 -0300

    Tests for attribute presence

--- spec/models/basic_health_unit_spec.rb
+++ spec/models/basic_health_unit_spec.rb
@@ -1,20 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe BasicHealthUnit, type: :model do
-  subject do
-    described_class.new(name: 'AMBULATORIO SANTA ANA',
-                        address: 'LINHA BOA ESPERANCA, DOBRADA',
-                        city: 'Sertão Santana',
-                        phone: '5134951206',
-                        latitude: '-30.4481470584861',
-                        longitude: '-51.6436600685105',
-                        unit_size_score: 3,
-                        adaptation_for_seniors_score: 1,
-                        medical_equipment_score: 2,
-                        medicine_score: 2)
-  end
+  subject { create(:basic_health_unit) }
 
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
+
+  it 'is not valid without a name' do
+    incomplete_subject = build(:basic_health_unit, name: nil)
+    expect(incomplete_subject).to_not be_valid
+  end
+
+  it 'is not valid without a address' do
+    incomplete_subject = build(:basic_health_unit, address: nil)
+    expect(incomplete_subject).to_not be_valid
+  end
+
+  it 'is not valid without a city' do
+    incomplete_subject = build(:basic_health_unit, city: nil)
+    expect(incomplete_subject).to_not be_valid
+  end
+
+  it 'is not valid without a phone' do
+    incomplete_subject = build(:basic_health_unit, phone: nil)
+    expect(incomplete_subject).to_not be_valid
+  end
+
+  it 'is not valid without a latitude' do
+    incomplete_subject = build(:basic_health_unit, latitude: nil)
+    expect(incomplete_subject).to_not be_valid
+  end
+
+  it 'is not valid without a longitude' do
+    incomplete_subject = build(:basic_health_unit, longitude: nil)
+    expect(incomplete_subject).to_not be_valid
+  end
+
+  it 'is not valid without a size_score' do
+    incomplete_subject = build(:basic_health_unit, size_score: nil)
+    expect(incomplete_subject).to_not be_valid
+  end
+
+  it 'is not valid without a adaptation_for_seniors_score' do
+    incomplete_subject = build(:basic_health_unit,
+                               adaptation_for_seniors_score: nil)
+    expect(incomplete_subject).to_not be_valid
+  end
+
+  it 'is not valid without a medical_equipment_score' do
+    incomplete_subject = build(:basic_health_unit, medical_equipment_score: nil)
+    expect(incomplete_subject).to_not be_valid
+  end
+
+  it 'is not valid without a medicine_score' do
+    incomplete_subject = build(:basic_health_unit, medicine_score: nil)
+    expect(incomplete_subject).to_not be_valid
+  end
+
 end
