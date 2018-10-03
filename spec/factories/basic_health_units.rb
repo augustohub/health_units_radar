f125b323d84feda232b2cac6157ee1833c444a64
Augusto Hubert <augustohub@gmail.com>
2018-10-03 19:07:05 -0300

    Changes static attributes to dynamic ones

--- spec/factories/basic_health_units.rb
+++ spec/factories/basic_health_units.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :basic_health_unit do
-    name 'AMBULATORIO SANTA ANA'
-    address 'LINHA BOA ESPERANCA, DOBRADA'
-    city 'Sertão Santana'
-    phone '5134951206'
-    latitude '-30.4481470584861'
-    longitude '-51.6436600685105'
-    unit_size_score 3
-    adaptation_for_seniors_score 1
-    medical_equipment_score 2
-    medicine_score 2
+    name                         { 'AMBULATORIO SANTA ANA' }
+    address                      { 'LINHA BOA ESPERANCA, DOBRADA' }
+    city                         { 'Sertão Santana' }
+    phone                        { '5134951206' }
+    latitude                     { '-30.4481470584861' }
+    longitude                    { '-51.6436600685105' }
+    unit_size_score              { 3 }
+    adaptation_for_seniors_score { 1 }
+    medical_equipment_score      { 2 }
+    medicine_score               { 2 }
   end
 end
