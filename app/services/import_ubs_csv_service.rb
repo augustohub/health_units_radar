01cbbc648317100a3b851d9780be5e1bc8395ff2
Augusto Hubert <augustohub@gmail.com>
2018-10-04 17:48:40 -0300

    Creates service to import ubs csv

---
+++ app/services/import_ubs_csv_service.rb
@@ -0,0 +1,35 @@
+require 'csv'
+
+class ImportUbsCsvService
+
+  def initialize
+    @csv_file = File.join(Rails.root, 'ubs.csv')
+  end
+
+  def run
+    counter = 0
+    CSV.foreach(@csv_file, headers: true) do |row|
+      ubs = BasicHealthUnit.create(name: row['nom_estab'],
+                                   address: [row['dsc_endereco'], row['dsc_bairro']].join(' - '),
+                                   city: row['dsc_cidade'],
+                                   phone: row['dsc_telefone'],
+                                   latitude: row['vlr_latitude'],
+                                   longitude: row['vlr_longitude'],
+                                   size_score: fetch_score(row['dsc_estrut_fisic_ambiencia']),
+                                   adaptation_for_seniors_score: fetch_score(row['dsc_adap_defic_fisic_idosos']),
+                                   medical_equipment_score: fetch_score(row['dsc_equipamentos']),
+                                   medicine_score: fetch_score(row['dsc_medicamentos']))
+
+      puts "LINE #{$NR} - #{ubs.errors.full_messages.join(', ')}" if ubs.errors.any?
+      counter += 1 if ubs.persisted?
+    end
+
+    puts
+  end
+
+  private
+
+  def fetch_score(text)
+    BasicHealthUnit::SCORE_TRANSLATION.key(text)
+  end
+end
