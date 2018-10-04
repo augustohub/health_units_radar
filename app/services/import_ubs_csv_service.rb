2291ca174c58d13cbaabad49f610aa3112578015
Augusto Hubert <augustohub@gmail.com>
2018-10-04 18:34:51 -0300

    Changes import service from class to module

--- app/services/import_ubs_csv_service.rb
+++ app/services/import_ubs_csv_service.rb
@@ -1,35 +1,35 @@
 require 'csv'
 
-class ImportUbsCsvService
+module ImportUbsCsvService
+  class << self
 
-  def initialize
-    @csv_file = File.join(Rails.root, 'ubs.csv')
-  end
+    def run
+      filename = File.join(Rails.root, 'ubs.csv')
+      counter = 0
 
-  def run
-    counter = 0
-    CSV.foreach(@csv_file, headers: true) do |row|
-      ubs = BasicHealthUnit.create(name: row['nom_estab'],
-                                   address: [row['dsc_endereco'], row['dsc_bairro']].join(' - '),
-                                   city: row['dsc_cidade'],
-                                   phone: row['dsc_telefone'],
-                                   latitude: row['vlr_latitude'],
-                                   longitude: row['vlr_longitude'],
-                                   size_score: fetch_score(row['dsc_estrut_fisic_ambiencia']),
-                                   adaptation_for_seniors_score: fetch_score(row['dsc_adap_defic_fisic_idosos']),
-                                   medical_equipment_score: fetch_score(row['dsc_equipamentos']),
-                                   medicine_score: fetch_score(row['dsc_medicamentos']))
+      CSV.foreach(filename, headers: true) do |row|
+        ubs = BasicHealthUnit.create(name: row['nom_estab'],
+                                     address: [row['dsc_endereco'], row['dsc_bairro']].join(' - '),
+                                     city: row['dsc_cidade'],
+                                     phone: row['dsc_telefone'],
+                                     latitude: row['vlr_latitude'],
+                                     longitude: row['vlr_longitude'],
+                                     size_score: fetch_score(row['dsc_estrut_fisic_ambiencia']),
+                                     adaptation_for_seniors_score: fetch_score(row['dsc_adap_defic_fisic_idosos']),
+                                     medical_equipment_score: fetch_score(row['dsc_equipamentos']),
+                                     medicine_score: fetch_score(row['dsc_medicamentos']))
 
-      puts "LINE #{$NR} - #{ubs.errors.full_messages.join(', ')}" if ubs.errors.any?
-      counter += 1 if ubs.persisted?
+        puts "LINE #{$NR} - #{ubs.errors.full_messages.join(', ')}" if ubs.errors.any?
+        counter += 1 if ubs.persisted?
+      end
+
+      puts "FINISHED. #{counter} UBSs imported!"
     end
 
-    puts
-  end
+    private
 
-  private
-
-  def fetch_score(text)
-    BasicHealthUnit::SCORE_TRANSLATION.key(text)
+    def fetch_score(text)
+      BasicHealthUnit::SCORE_TRANSLATION.key(text)
+    end
   end
 end
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -2,7 +2,6 @@ namespace :import do
 
   desc "Import 'Unidades Básicas de Saúde' (UBS) from csv"
   task basic_health_units: :environment do
-    import_task = ImportUbsCsvService.new
-    import_task.run
+    ImportUbsCsvService.run
   end
 end
