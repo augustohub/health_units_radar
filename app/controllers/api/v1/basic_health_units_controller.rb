2d05b12b81c4e0db89f90bd64f37c1cd71ce3be2
Augusto Hubert <augustohub@gmail.com>
2018-10-16 18:11:49 -0300

    Changes to meet new specs

--- app/controllers/api/v1/basic_health_units_controller.rb
+++ app/controllers/api/v1/basic_health_units_controller.rb
@@ -1,24 +1,27 @@
 module Api::V1
   class BasicHealthUnitsController < ApiController
-    before_action :decompose_geocode, only: :index
 
     # GET /api/v1/find_ubs
     def index
-      ubs = BasicHealthUnit.by_distance(origin: [@lat, @lon])
-                           .page(ubs_params[:page])
-                           .per(ubs_params[:per_page])
+      if params[:query].present?
+        ubs = BasicHealthUnit.by_distance(origin: decompose_geocode)
+                             .page(ubs_params[:page])
+                             .per(ubs_params[:per_page])
 
-      render json: ubs,
-             adapter: :json,
-             meta: { pagination: { page: ubs.current_page,
-                                   per_page: ubs.limit_value,
-                                   total_entries: BasicHealthUnit.count } }
+        render json: ubs,
+               adapter: :json,
+               meta: { pagination: { page: ubs.current_page,
+                                     per_page: ubs.limit_value,
+                                     total_entries: BasicHealthUnit.count } }
+      else
+        render json: '', status: :unprocessable_entity
+      end
     end
 
     private
 
     def decompose_geocode
-      @lat, @lon = ubs_params[:query].split(', ')
+      ubs_params[:query].split(',')
     end
 
     def ubs_params
